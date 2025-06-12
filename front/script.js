6e7b34865e8ff63ecd600c2791a117870b547292
Sebastian <juans-betancurt@unilibre.edu.co>
2025-06-12 18:35:35 -0500

    Front created

---
+++ front/script.js
@@ -0,0 +1,20 @@
+// Reference for HTML elements
+const searchInput = document.getElementById('searchInput');
+const searchButton = document.getElementById('searchButton');
+const resultsContainer = document.getElementById('resultsContainer');
+const messageArea = document.getElementById('messageArea');
+
+
+// Displays the {message} and the {type} of message
+function displayMessage(message, type = 'info') {
+    messageArea.textContent = message;
+    messageArea.className = 'message ' + type;
+}
+
+// Clear results container and message area
+function clearResults() {
+    resultsContainer.innerHTML = '';
+    messageArea.textContent = '';
+    messageArea.className = 'message';
+}
+
