bdbeb91502fd1c9e9863e5eaa962a90526f96659
Sebastian <juans-betancurt@unilibre.edu.co>
2025-06-12 16:43:13 -0500

    First commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+
+
+# App
+app = FastAPI()
+
+
+@app.get("/")
+def root():
+    return {"message": "Hello, World!"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
